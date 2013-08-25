bacfeeaa3e62f09fb6d4117f2d91fe9651e2686b
Yowasou <jun.makita@gmail.com>
2013-08-25 14:56:08 +0900

    aa

---
+++ CommitedStepGetter/CommiedStep.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitedStepGetter
+{
+    public class CommitedStep
+    {
+        public virtual string Execute(string[] args)
+        {
+            string repoPath = args[1];
+            string revNo = args[2];
+
+            string svnpath = CommitedStepGetter.Properties.Settings.Default.svnpath;
+            string svnlook = svnpath + "\\" + CommitedStepGetter.Properties.Settings.Default.svnlook;
+
+            //変更されたファイルの一覧を取得
+            string changedFiles = GetCommandResult(svnlook, "changed " + repoPath + " -r " + revNo);
+
+            Debug.Write(output); // ［出力］ウィンドウに出力
+            return "1";
+        }
+        public virtual string GetCommandResult(string command, string arg)
+        {
+            ProcessStartInfo psInfo = new ProcessStartInfo();
+            psInfo.FileName = command; // 実行するファイル
+            psInfo.CreateNoWindow = true; // コンソール・ウィンドウを開かない
+            psInfo.UseShellExecute = false; // シェル機能を使用しない
+            psInfo.Arguments = arg;
+            psInfo.RedirectStandardOutput = true; // 標準出力をリダイレクト
+            Process p = Process.Start(psInfo); // アプリの実行開始
+            string output = p.StandardOutput.ReadToEnd(); // 標準出力の読み取り
+            output = output.Replace("\r\r\n", "\n"); // 改行コードの修正
+            return output;
+        }
+    }
+}
---
+++ CommitedStepGetter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitedStepGetter
+{
+    class Program
+    {
+        /// <summary>
+        /// 0 : バッチファイルのあるフォルダ
+        /// 1 : コミットしたローカルのリポジトリパス
+        /// 2 : コミットしたリビジョン番号
+        /// 3 : ???（前回リビジョン - a?）
+        /// cd "C:\Program Files (x86)\SourceMonitor"
+        /// SourceMonitor.exe /DCs C:\temp\testcode\temp.cs
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            CommitedStep cs = new CommitedStep();
+            Console.WriteLine(cs.Execute(args));
+            return;
+        }
+    }
+}
