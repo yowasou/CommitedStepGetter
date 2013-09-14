7cdd6fb550efc97efef2a29b8855535a0b37b662
Yowasou <jun.makita@gmail.com>
2013-09-14 16:09:31 +0900

    出力サンプル

---
+++ output_sample/post-commit.bat
@@ -0,0 +1,8 @@
+rem # 0 : バッチファイルのあるフォルダ
+rem # 1 : コミットしたローカルのリポジトリパス
+rem # 2 : コミットしたリビジョン番号
+rem # 3 : ???（前回リビジョン - a?）
+
+C:\Repositories\svntest\hooks\CommitedStepGetter.exe %0 %1 %2 %3
+
+exit 0
